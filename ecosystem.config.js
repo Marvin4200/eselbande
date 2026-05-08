9351a1a75db8cd10edaf5491cbb51f43257cb892
Txxle <marvin.haertel3@gmail.com>
2026-05-08 13:34:04 +0200

    ops: add pm2 restart delay and max restart cap

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,8 @@ module.exports = {
         instances: 1,
         exec_mode: 'fork',
         autorestart: true,
+        restart_delay: 5000,
+        max_restarts: 10,
         watch: false,
         max_memory_restart: '500M',
         env: {
